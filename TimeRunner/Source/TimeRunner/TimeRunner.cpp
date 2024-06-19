bc14eb4615aaf86f9e86b04607851169463baa84
Johyechan <98877897+Johyechan@users.noreply.github.com>
2024-06-19 14:37:40 +0900

    Make Project & Map

---
+++ TimeRunner/Source/TimeRunner/TimeRunner.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TimeRunner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TimeRunner, "TimeRunner" );
---
+++ TimeRunner/Source/TimeRunner/TimeRunnerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TimeRunnerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIMERUNNER_API ATimeRunnerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
